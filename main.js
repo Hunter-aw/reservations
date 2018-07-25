312aaec6bdfe34a752d3dfc70787b17532dfc309
hunter-aw <hunteraweinsheink@gmail.com>
2018-07-25 17:13:10 +0300

    worked on the reservations script with objects and case insensitvity

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 var reservations = {
-  'Bob': { claimed: false },
-  'Ted': { claimed: true }
+  'Bob': {
+    claimed: false
+  },
+  'Ted': {
+    claimed: true
+  }
 }
 
-var name = prompt('Please enter the name for your reservation');
+var isName = prompt('Please enter the name for your reservation');
+let name =  new RegExp(isName, "i");
 
-var claimReservation = function () {
-  // write your code here!
+var claimReservation = function () {  
+  if ((reservations[name]) && (reservations[name].claimed) === false) {
+    return (alert("Welcome to George and Sons, we have your table ready"))
+  } else if ((reservations[name]) && (reservations[name].claimed === true)) {
+    return alert("well this is awkward, someone's taken your table")
+  } else {
+    reservations[name] = {claimed: true}
+    return alert(`We have plentry of room, come on in ${name}!`)
+  }
 }
 
 claimReservation();
